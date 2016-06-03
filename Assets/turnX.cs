001eb41cc7b9a420cfa63aeb1a7c1ab59fbae30c
maroo1116 <maroo1116@korea.ac.kr>
2016-06-03 23:24:05 +0900

    1인칭 이동 + 마우스 이동
    아직 별거 없음

---
+++ Assets/turnX.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turnX : MonoBehaviour {
+    float TurnSpeed;
+    Vector3 V3;
+    // Use this for initialization
+    void Start () {
+        TurnSpeed = 2f;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        V3 = new Vector3(0, Input.GetAxis("Mouse X"), 0);
+        transform.Rotate(V3 * TurnSpeed);
+
+    }
+}
---
+++ Assets/turnY.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turnY : MonoBehaviour {
+    float TurnSpeed;
+    Vector3 V3;
+    // Use this for initialization
+    void Start()
+    {
+        TurnSpeed = 2f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        V3 = new Vector3(-Input.GetAxis("Mouse Y"), 0, 0);
+        transform.Rotate(V3 * TurnSpeed);
+
+    }
+}
